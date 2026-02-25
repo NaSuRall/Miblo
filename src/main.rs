ce78a7d4bea38c7acffb216103b867abe9b3450a
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-02-25 14:10:23 +0000

    ADD Clap to configure CLI for the App

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Commands {
+    CreateApp { name: String },
+}
+
+#[derive(Parser)]
+#[command(name = "miblo", about = "CLI pour générer des API")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::CreateApp { name } => {
+            println!("Création de l'application {}", name);
+        }
+    }
 }
