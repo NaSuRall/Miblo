51c845b6861c6169ccad7fd6ca5c9da3849b3480
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-04-02 08:40:05 +0000

    FIX setup and delete /rust api test

--- rust/src/models/user.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Serialize, Deserialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct User {
-        pub last_name: String,
-        pub email: String,
-        pub password: i32,
-        pub test: String,
-}
