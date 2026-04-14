0c2a6feaee5ff969c9f6b4551757d51063d0d15c
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-04-14 08:30:44 +0000

    FIX handler_name_low for database on SELECT / handler_generator

--- src/generator/generator_handler.rs
+++ src/generator/generator_handler.rs
@@ -13,7 +13,8 @@ pub fn generate_handler(models: &Vec<Value>) -> Vec<(String, String)>{
         let name = model["name"].as_str().expect("Model name is not a string");
         let result = format!("{}{}", &name[..1].to_uppercase(), &name[1..]);
         let data = json!({
-            "handler_name": result
+            "handler_name": result,
+            "handler_name_low": name.to_lowercase()
         });
 
         let rendered = handlebars
