bb9f72ede884e9bd79741ef2775b231959f029ef
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-04-27 12:24:50 +0000

    ADD fn to request create folder api at start and impl to cli mod.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,11 +2,12 @@ use std::env;
 use std::path::PathBuf;
 pub mod config;
 use crate::engine::fs::copy_dir_all;
+use crate::engine::global_fn::request_cretate_folder;
 use crate::generator::generator_routes;
+use crate::generator::generator_sql;
+use crate::generator::generator_sqlx;
 use crate::generator::generator_template;
 use crate::generator::generator_yaml;
-use crate::generator::generator_sqlx;
-use crate::generator::generator_sql;
 use crate::parser;
 use crate::runtime;
 use crate::writer::writer_handlers;
@@ -14,18 +15,24 @@ use crate::writer::writer_models;
 use clap::{Parser, Subcommand};
 use colored::*;
 
-
 #[derive(Subcommand)]
 enum Commands {
-    Init { 
+    Init {
         #[arg(short, long)]
-        name: String, 
+        name: String,
         #[arg(short, long)]
-        template_dir: PathBuf
+        template_dir: PathBuf,
     },
-    Run { name: String },
-    Generate { name: String },
-    Export { name: String, destination: PathBuf }
+    Run {
+        name: String,
+    },
+    Generate {
+        name: String,
+    },
+    Export {
+        name: String,
+        destination: PathBuf,
+    },
 }
 
 #[derive(Parser)]
@@ -39,36 +46,40 @@ pub fn lunch() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     match cli.command {
-        // NAME : Nom du projet 
+        // NAME : Nom du projet
         // TEMPLATE_DIR : chemin vers template a utiliser
         Commands::Init { name, template_dir } => {
-            // Création du Dossier ( nom de l'api)
-            
-            // NOTE : RENDRE CELA VARIABLE 
+            // NOTE : RENDRE CELA VARIABLE
             let current_dir = std::env::current_dir()?;
             let project_path = current_dir.join(&name);
-            std::fs::create_dir_all(&project_path)?;
-            // Vérification si le fichier de l'api 
+            request_cretate_folder(&project_path)?;
+
+            // Vérification si le fichier de l'api
             // a bien ete crée ou pas
-            if ! std::fs::exists(&template_dir)? {
+            if !std::fs::exists(&template_dir)? {
                 return Err("Err de caca".into());
             }
-                    
-            // Lecture du Fichier Route.yaml 
-            let json_value = parser::reader_route(&template_dir)?;
-            let miblo_config = generator_yaml::reader_json(template_dir.parent().unwrap().to_path_buf(), json_value)?;
 
-            // GENERATEUR DE STRUCTURE 
+            // Lecture du Fichier Route.yaml
+            let json_value = parser::reader_route(&template_dir)?;
+            let miblo_config = generator_yaml::reader_json(
+                template_dir.parent().unwrap().to_path_buf(),
+                json_value,
+            )?;
+
+            // GENERATEUR DE STRUCTURE
             // [ ROUTES , MODEL , HANDLERS , MIGRATION , SQL]
             generator_template::generator(&project_path, &name, &miblo_config)?;
             generator_sqlx::generator(&project_path, &miblo_config);
             writer_models::write_model(&project_path, &miblo_config)?;
-            generator_sql::generator(&project_path , &miblo_config); 
+            generator_sql::generator(&project_path, &miblo_config);
             generator_routes::generate_routes(&project_path, &miblo_config);
             writer_handlers::write_handlers(&project_path, &miblo_config)?;
 
-
-            println!("{}", "Structure de base générée [ MODELS, ROUTES, HANDLERS, ROUTE.YAML ]".green());
+            println!(
+                "{}",
+                "Structure de base générée [ MODELS, ROUTES, HANDLERS, ROUTE.YAML ]".green()
+            );
             println!("{}", "Vous pouvez maintenant acceder a au fichier [route.yaml] pour y modifier les données !".yellow());
         }
 
@@ -92,7 +103,6 @@ pub fn lunch() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         Commands::Export { name, destination } => {
-    
             // trouver le chemin de l'api generer
             let current_dir = env::current_dir()?;
             let project_path = current_dir.join(name);
--- src/engine/global_fn.rs
+++ src/engine/global_fn.rs
@@ -1,10 +1,23 @@
+use colored::*;
+use std::path::PathBuf;
+pub fn request_cretate_folder(project_path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", "Create your api folder here ? y/n ".green());
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    if input.trim() != "y" {
+        return Err("Aborted".into());
+    }
+    std::fs::create_dir_all(project_path)?;
+    Ok(())
+}
+
 pub fn map_type(t: &str) -> &str {
-   match t {
-      "String" | "string" => "String",
-      "Int" | "int" => "i32",
-      "Binary" => "u8",
-      _ => "String",
-   }
+    match t {
+        "String" | "string" => "String",
+        "Int" | "int" => "i32",
+        "Binary" => "u8",
+        _ => "String",
+    }
 }
 
 pub fn map_type_sql(rust_type: &str) -> &str {
@@ -16,7 +29,6 @@ pub fn map_type_sql(rust_type: &str) -> &str {
         "f32" => "REAL",
         "f64" => "DOUBLE PRECISION",
         "u8" => "BINARY(16)",
-      _ => "TEXT", // fallback
-      
+        _ => "TEXT", // fallback
     }
 }
